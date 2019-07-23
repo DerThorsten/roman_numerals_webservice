0ae0f958652ac122bd8fcc6d65058fdb55c92d1b
DerThorsten <derthorstenbeier@gmail.com>
2019-07-23 23:39:13 +0200

    changed notebook

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     include_package_data=True,
     keywords='roman_numerals_webservice',
     name='roman_numerals_webservice',
-    packages=find_packages(include=['roman_numerals_webservice']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
